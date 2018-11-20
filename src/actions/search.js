f9ce185c4eb6e5f6871584e97ff9f497d79be206
Duncan Wallace <duncwa@gmail.com>
2018-11-19 16:01:34 -0800

    In progress

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,13 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+    var data;
   //TODO:  Write an asynchronous action to handle a video search!
+  searchYouTube({YOUTUBE_API_KEY, q}, (results) => data = results);
+  changeVideoList(data);
+  changeVideo(data);
+
+
 };
 
 export default handleVideoSearch;
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
+import handleVideoSearch from '../actions/search.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 
@@ -11,3 +12,5 @@ export default store = (initialState) => {
     applyMiddleware(thunk)
   );
 }
+
+store.dispatch(handleVideoSearch);
